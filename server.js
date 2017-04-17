ba38aaf80ced92c71b9c43fe0907d9fafa9fb49e
Carlos Cadena <cs.cadena@gmail.com>
2017-04-17 16:37:05 -0700

    server having issues

---
+++ server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const pg = require('pg');
+const fs = require('fs');
+const express = require('express');
+//const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const conString = 'postgres://localhost:5432';
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', function(error) {
+  console.error(error);
+});
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
+
+// function loadUsers() {
+//   fs.readFile('./public/data/table.json', (err, fd) => {
+//     JSON.parse(fd.toString()).forEach(ele => {
+//       client.query(
+//         'INSERT INTO users(user, groups) VALUES($1, $2) ON CONFLICT DO NOTHING',
+//         [ele.user, ele.groups]
+//       )
+//       .catch(console.error);
+//       // client.query(
+//       //   'ALTER TABLE users ADD VALUES($1)',
+//       //   [ele.user]
+//       // )
+//       // .catch(console.error);
+//     })
+//   })
+// }
+
+function loadDB() {
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    users (
+      user_id SERIAL PRIMARY KEY,
+      user,
+      groups VARCHAR(255),
+      wins,
+      games
+    );`
+  )
+  // .then(loadUsers)
+  .catch(console.error);
+}
+
+loadDB();
