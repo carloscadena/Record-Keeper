1742c37912574faf75d2674a36ee76e271a37d61
W. Ely Paysinger <paysinger@gmail.com>
2017-04-22 10:16:29 -0700

    working on a linked database rather than one with growing columns

--- server.js
+++ server.js
@@ -24,11 +24,28 @@ app.get('/', (request, response) => response.sendFile('index.html', {root: '.'})
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
+
+// ------- SEED database while in development ------ //
 function loadUsers() {
-  fs.readFile('./public/data/table.json', (err, fd) => {
+  fs.readFile('./public/data/users.json', (err, fd) => {
     JSON.parse(fd.toString()).forEach(ele => {
       client.query(
-        `INSERT INTO users(user_name, groups, wins, games_played) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING`,
+        `INSERT INTO users(user_name, groups, wins, losses, games_played) VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING`,
+        [ele.user, ele.groups, ele.wins, ele.losses, ele.games_played]
+      )
+      .catch(console.error);
+    });
+  });
+}
+function loadGames() {
+  fs.readFile('./public/data/games.json', (err, fd) => {
+    JSON.parse(fd.toString()).forEach(ele => {
+      client.query(
+        `INSERT INTO
+            games(winner, loser, winner_score, loser_score)
+            VALUES(SELECT, SELECT, $1, $2)SELECT author_id, $1, $2, $3, $4
+            FROM authors
+            WHERE author=$5;`,
         [ele.user, ele.groups, ele.wins, ele.games_played]
       )
       .catch(console.error);
@@ -40,7 +57,7 @@ function loadDB() {
     `CREATE TABLE IF NOT EXISTS
     users (
       user_id SERIAL PRIMARY KEY,
-      user_name VARCHAR(50) UNIQUE NOT NULL,
+      user_name VARCHAR(50),
       groups TEXT[],
       wins INT,
       games_played INT
@@ -49,6 +66,19 @@ function loadDB() {
   .then(loadUsers)
   .catch(console.error);
   // TODO Create new table where games are rows and p1, p2 and date are fields
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    games (
+      game_id SERIAL PRIMARY KEY,
+      winner INTEGER NOT NULL REFERENCES users(user_id),
+      loser INTEGER NOT NULL REFERENCES users(user_id),
+      winner_score INT NOT NULL,
+      loser_score INT NOT NULL,
+      date DATE DEFAULT GETDATE()
+    );`
+  )
+  .then(loadGames)
+  .catch(console.error);
 }
 
 loadDB();
