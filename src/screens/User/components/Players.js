d621b216210f47f9eeca44d1b3e082659d0a1b55
W. Ely Paysinger <paysinger@gmail.com>
2017-04-26 17:54:46 -0700

    added some click handling

--- src/screens/User/components/Players.js
+++ src/screens/User/components/Players.js
@@ -3,15 +3,22 @@ import React, {Component} from 'react';
 export default class Players extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      focused: ''
+    }
+  }
+  clicked(id){
+    this.setState({focused: id});
   }
   render() {
+    let self = this;
     return (
       <div>{ this.props.players.map(
-        function(ele, index){
-          return <div id={ele.id} key={index.toString()}>
-            <span className="name">{ele.user_name}</span>
-            <span className="wins">Wins {ele.wins}</span>
-            <span className="losses">Losses {ele.losses}</span>
+        function(ele){
+          return <div id={ele.id} key={ele.id.toString()} onClick={self.clicked.bind(self, ele.id)}>
+              <span className="name">{ele.user_name}</span>
+              <span className="wins">Wins {ele.wins}</span>
+              <span className="losses">Losses {ele.losses}</span>
           </div>;
         }) }
       </div>
