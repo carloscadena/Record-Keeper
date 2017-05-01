52ff17b8a100f5c324bd7d226eb0a7b41786e81e
W. Ely Paysinger <paysinger@gmail.com>
2017-05-01 14:07:47 -0700

    win loss event handlers now log to console

--- src/components/Opponent.js
+++ src/components/Opponent.js
@@ -4,13 +4,22 @@ export default class Opponent extends React.Component {
   constructor(props) {
     super(props);
   }
+  clicked(event){
+    console.log(event)
+  }
   render() {
     return (
       <div>
         <div>Opponent Id is {this.props.opponent} and Name is {this.props.name}</div>
         <ul>
-          <li >I won</li>
-          <li >I Lost</li>
+          <li id="win" onClick={
+            this.clicked
+            // this.props.select("win")
+           }>I won</li>
+          <li id="loss" onClick={
+            this.clicked
+            // this.props.select('loss')
+           }>I Lost</li>
         </ul>
         <button type="submit">Submit</button>
       </div>
